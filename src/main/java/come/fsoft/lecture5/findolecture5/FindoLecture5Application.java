906f60f5b713d02eeaf9a441f808b83377445639
trieunv <>
2024-06-24 02:23:23 +0700

    init project

---
+++ src/main/java/come/fsoft/lecture5/findolecture5/FindoLecture5Application.java
@@ -0,0 +1,13 @@
+package come.fsoft.lecture5.findolecture5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindoLecture5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindoLecture5Application.class, args);
+    }
+
+}
